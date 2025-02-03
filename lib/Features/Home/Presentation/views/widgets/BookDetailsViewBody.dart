321e97f3d8382ab647575be7358380db2627dc03
osamamohamedM <osamamoahmed@gmail.com>
2025-02-03 23:28:59 +0200

    Add Cutstom Scroll View

--- lib/Features/Home/Presentation/views/widgets/BookDetailsViewBody.dart
+++ lib/Features/Home/Presentation/views/widgets/BookDetailsViewBody.dart
@@ -6,33 +6,53 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'BuildCustomButton.dart';
 
-
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        BookImage(width: width),
-        const BookDescription(),
-        const SizedBox(height: 12,),
-        const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
-        const SizedBox(height: 12,),
-        const BuildCustomButton(),
-        const SizedBox(height: 12,),
-        Align(alignment:Alignment.topLeft ,child: Text('You can also like' , style: Styles.textStyle20.copyWith(fontWeight: FontWeight.w700),)),
-        const SizedBox(height: 12,),
-        const BookListView(factor: 0.18),
-        const SizedBox(height: 40,),
-        
+    return CustomScrollView(
+      
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              BookImage(width: width),
+              const BookDescription(),
+              const SizedBox(
+                height: 12,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const BuildCustomButton(),
+              const Expanded(
+                  child: SizedBox(
+                height: 12,
+              )),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.w700),
+                  )),
+              const SizedBox(
+                height: 12,
+              ),
+              const BookListView(factor: 0.18),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
 }
-
-
-
-
-
