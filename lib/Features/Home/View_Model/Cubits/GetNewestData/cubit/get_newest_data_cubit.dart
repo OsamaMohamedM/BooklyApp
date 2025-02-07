64a6d948c23aa2d6a27a82cb250c612febea4625
osamamohamedM <osamamoahmed@gmail.com>
2025-02-07 21:18:53 +0200

    Creat The Newest Books Cubit

--- lib/Features/Home/View_Model/Cubits/GetNewestData/cubit/get_newest_data_cubit.dart
+++ lib/Features/Home/View_Model/Cubits/GetNewestData/cubit/get_newest_data_cubit.dart
@@ -1,8 +1,17 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
+import 'package:bookly_app/Features/Home/Data/Models/BookModel/BookModel.dart';
+import 'package:bookly_app/Features/Home/Data/Repository/HomeRepo.dart';
+import 'package:bookly_app/Features/Home/Data/Repository/HomeRepoImp.dart';
+import 'package:bookly_app/core/utils/ApisServices.dart';
 part 'get_newest_data_state.dart';
 
 class GetNewestDataCubit extends Cubit<GetNewestDataState> {
+  HomeRepo homeRepo = HomeRepoImp(Apisservices());
   GetNewestDataCubit() : super(GetNewestDataInitial());
+
+  Future<void> getNewestBooks() async {
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((Failrue) => emit(GetNewestDataFaliure(Failrue.message)),
+        (Books) => emit(GetNewestDataSuccess(Books)));
+  }
 }
--- lib/Features/Home/View_Model/Cubits/GetNewestData/cubit/get_newest_data_state.dart
+++ lib/Features/Home/View_Model/Cubits/GetNewestData/cubit/get_newest_data_state.dart
@@ -3,3 +3,15 @@ part of 'get_newest_data_cubit.dart';
 abstract class GetNewestDataState {}
 
 final class GetNewestDataInitial extends GetNewestDataState {}
+
+final class GetNewestDataSuccess extends GetNewestDataState {
+  List<BookModel> Books;
+  GetNewestDataSuccess(this.Books);
+}
+
+final class GetNewestDataLoding extends GetNewestDataState {}
+
+final class GetNewestDataFaliure extends GetNewestDataState {
+  String message;
+  GetNewestDataFaliure(this.message);
+}
