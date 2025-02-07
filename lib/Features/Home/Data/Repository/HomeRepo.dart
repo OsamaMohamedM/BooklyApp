5abfad4a918924148b315bfc115c8a01b3280f30
osamamohamedM <osamamoahmed@gmail.com>
2025-02-07 18:41:02 +0200

    Fetch Books

--- lib/Features/Home/Data/Repository/HomeRepo.dart
+++ lib/Features/Home/Data/Repository/HomeRepo.dart
@@ -4,6 +4,6 @@ import 'package:dartz/dartz.dart';
 import '../Models/BookModel/BookModel.dart';
 
 abstract class HomeRepo {
-  Future<Either<Faliure , List<BookModel>>> fetchBestSallerBook();
-  Future<Either<Faliure , List<BookModel>>> fetchFeaturedBook();
+  Future<Either<Faliure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/Features/Home/Data/Repository/HomeRepoImp.dart
+++ lib/Features/Home/Data/Repository/HomeRepoImp.dart
@@ -1,17 +1,27 @@
 import 'package:bookly_app/Features/Home/Data/Models/BookModel/BookModel.dart';
 import 'package:bookly_app/Features/Home/Data/Repository/HomeRepo.dart';
 import 'package:bookly_app/core/errors/Faliure.dart';
+import 'package:bookly_app/core/utils/ApisServices.dart';
 import 'package:dartz/dartz.dart';
 
-class Homerepoimp extends HomeRepo{
+class Homerepoimp extends HomeRepo {
+  final Apisservices apisservices = Apisservices();
   @override
-  Future<Either<Faliure, List<BookModel>>> fetchBestSallerBook() {
-    
+  Future<Either<Faliure, List<BookModel>>> fetchNewestBooks() async {
+    try{
+      
+    var response = await apisservices.get(
+        'volumes?Filtering=free-ebook&Sorting=newest &q=subject:Programming');
+    List<BookModel> books = (response['items'] as List<dynamic>)
+        .map((item) => BookModel.fromJson(item))
+        .toList();
+    return right(books);
+  }catch(e)
+  {
+
+  }
   }
 
   @override
-  Future<Either<Faliure, List<BookModel>>> fetchFeaturedBook() {
-    
-  }
-  
+  Future<Either<Faliure, List<BookModel>>> fetchFeaturedBooks() {}
 }
